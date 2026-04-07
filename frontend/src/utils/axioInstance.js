58996f1ec1b55d52c09a1caec4286ba264e06da4
Abdullah Al Noman <140273645+nomanstine@users.noreply.github.com>
2026-04-07 05:06:29 +0000

    Update axioInstance.js

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
-  withCredentials: true,
+  // withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
